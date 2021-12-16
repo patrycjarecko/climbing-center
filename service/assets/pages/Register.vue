b30690d0fb32f48f9a0560f8573d8b777d3c8b6c
Kasper Seweryn <github@wvffle.net>
2021-12-17 00:40:53 +0100

    feat: add register logic

--- service/assets/pages/Register.vue
+++ service/assets/pages/Register.vue
@@ -4,7 +4,8 @@
       <img class="w-full" src="https://images.unsplash.com/photo-1571649123442-dc0e1965e037?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80">
       <div class="bg-gray-600 opacity-50 absolute inset-0"></div>
     </div>
-    <div class="bg-white rounded-lg shadow-xl p-6 min-w-2xl relative z-1">
+    <div class="bg-white rounded-lg shadow-xl p-6 min-w-2xl relative z-1 overflow-hidden">
+      <it-progressbar v-if="loading" infinite :height="4" class="absolute top-0 left-0" />
       <div class="flex pb-8">
         <div class="w-8 flex items-center text-gray-400">
           <icon-user class="w-6 h-6" />
@@ -33,7 +34,7 @@
           </div>
 
           <div >
-            <it-button>Register</it-button>
+            <it-button @click="register">Register</it-button>
           </div>
         </div>
       </div>
@@ -43,12 +44,49 @@
 
 <script setup>
 import IconUser from '~icons/mdi/user'
-import { ref } from 'vue'
+import { reactive, ref, watchEffect } from 'vue'
+import gql from 'graphql-tag'
+import { useMutation } from '@vue/apollo-composable'
+import { useRouter } from 'vue-router'
+import { Message } from 'equal-vue'
+
+const router = useRouter()
 
 const firstname = ref('')
 const lastname = ref('')
 const email = ref('')
 const date = ref('')
+
+const { mutate, loading, onDone, onError } = useMutation(gql`mutation createClient($firstname: String!, $lastname: String!, $email: String!, $date: Date!) {
+    createClient(input: { firstName: $firstname, lastName: $lastname, email: $email, birthday: $date }) {
+      client {
+        cardNumber
+      }
+    }
+  }`, {
+  variables: reactive({
+    firstname,
+    lastname,
+    email,
+    date
+  })
+})
+
+onDone(client => {
+  router.push({ name: 'Clients' })
+})
+
+onError(err => {
+  Message.danger({
+    text: 'An error occured, check the console.'
+  })
+
+  console.error(err)
+})
+
+const register = async () => {
+  mutate()
+}
 </script>
 
 
