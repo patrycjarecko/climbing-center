d3bf4a8914025fbffe058c4a0dda304f8c75c999
Kasper Seweryn <github@wvffle.net>
2022-01-20 22:42:29 +0100

    feat: add deleting clients

--- service/assets/pages/Clients.vue
+++ service/assets/pages/Clients.vue
@@ -18,36 +18,51 @@
         </tr>
 
         <tbody class="divide-y bg-white">
-          <tr v-if="!clients.length">
-            <td colspan="6" class="p-4 text-center text-gray-400">No results</td>
-          </tr>
-          <tr v-for="client of clients">
-            <td class="p-4">{{ client.cardNumber }}</td>
-            <td class="p-4">{{ client.firstName }}</td>
-            <td class="p-4">{{ client.lastName }}</td>
-            <td class="p-4">{{ client.email }}</td>
-            <td class="p-4">{{ client.birthday }}</td>
-            <td class="p-4 text-right">
-              <edit-icon class="text-gray-500 hover:text-blue-400 cursor-pointer mr-4" />
-              <trash-icon class="text-gray-500 hover:text-red-500 cursor-pointer" />
-            </td>
-          </tr>
+        <tr v-if="!clients.length">
+          <td colspan="6" class="p-4 text-center text-gray-400">No results</td>
+        </tr>
+        <tr v-for="client of clients">
+          <td class="p-4">{{ client.cardNumber }}</td>
+          <td class="p-4">{{ client.firstName }}</td>
+          <td class="p-4">{{ client.lastName }}</td>
+          <td class="p-4">{{ client.email }}</td>
+          <td class="p-4">{{ client.birthday }}</td>
+          <td class="p-4 text-right">
+            <edit-icon class="text-gray-500 hover:text-blue-400 cursor-pointer mr-4" />
+            <trash-icon @click="deleteClient(client)" class="text-gray-500 hover:text-red-500 cursor-pointer" />
+          </td>
+        </tr>
         </tbody>
       </table>
     </div>
+
+    <it-modal v-model="confirmDelete">
+      <template #header>
+        <h3>Delete client</h3>
+      </template>
+      <template #body>
+        Client will be deleted permanently, please confirm
+      </template>
+      <template #actions>
+        <it-button @click="confirmDelete = null">Cancel</it-button>
+        <it-button type="danger" :loading="deleting" @click="deleteClient" >Delete</it-button>
+      </template>
+    </it-modal>
   </div>
 </template>
 
 <script setup>
 import EditIcon from '~icons/mdi/pencil'
 import TrashIcon from '~icons/mdi/trash'
-import { useQuery } from '@vue/apollo-composable'
+import { useMutation, useQuery } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+import { until } from '@vueuse/core'
 
-const { result } = useQuery(gql`
+const { result, refetch } = useQuery(gql`
     query {
       clients {
+        cardNumber
         firstName
         lastName
         birthday
@@ -55,6 +70,35 @@ const { result } = useQuery(gql`
       }
     }
 `)
+const { mutate: deleteClientMutation } = useMutation(gql`
+    mutation deleteClient($cardNumber: String!) {
+        deleteClient(id: $cardNumber) {
+            client {
+                cardNumber
+            }
+        }
+    }
+`)
 
 const clients = computed(() => result.value?.clients ?? [])
+
+const deleting = ref(false)
+const clientToDelete = ref()
+const confirmDelete = ref(false)
+const deleteClient = async (client = null) => {
+  if (client && !(client instanceof MouseEvent)) {
+    clientToDelete.value = client
+    confirmDelete.value = true
+    return
+  }
+
+  if (clientToDelete.value) {
+    deleting.value = true
+    await deleteClientMutation({ cardNumber: clientToDelete.value.cardNumber })
+    await refetch()
+    deleting.value = false
+    clientToDelete.value = null
+    confirmDelete.value = false
+  }
+}
 </script>
